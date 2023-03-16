ec04f9e930eb6c9ced25dab63c19dd166bb27e58
mdhralif <hasiburrahman999.alif@gmail.com>
2023-03-16 08:55:56 +0600

    Login_Page

---
+++ Authentication/Authentication/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace Authentication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Username = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.user_textbox = new System.Windows.Forms.TextBox();
+            this.pwtextbox = new System.Windows.Forms.TextBox();
+            this.loginbutton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.Location = new System.Drawing.Point(215, 110);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(94, 20);
+            this.Username.TabIndex = 0;
+            this.Username.Text = "Username";
+            this.Username.Click += new System.EventHandler(this.Username_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(215, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password";
+            // 
+            // user_textbox
+            // 
+            this.user_textbox.Location = new System.Drawing.Point(342, 110);
+            this.user_textbox.Name = "user_textbox";
+            this.user_textbox.Size = new System.Drawing.Size(187, 22);
+            this.user_textbox.TabIndex = 2;
+            // 
+            // pwtextbox
+            // 
+            this.pwtextbox.Location = new System.Drawing.Point(342, 154);
+            this.pwtextbox.Name = "pwtextbox";
+            this.pwtextbox.Size = new System.Drawing.Size(187, 22);
+            this.pwtextbox.TabIndex = 3;
+            // 
+            // loginbutton
+            // 
+            this.loginbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.loginbutton.ForeColor = System.Drawing.Color.White;
+            this.loginbutton.Location = new System.Drawing.Point(342, 205);
+            this.loginbutton.Name = "loginbutton";
+            this.loginbutton.Size = new System.Drawing.Size(187, 44);
+            this.loginbutton.TabIndex = 4;
+            this.loginbutton.Text = "Login";
+            this.loginbutton.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(189, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Don\'t have an account?";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Red;
+            this.linkLabel1.Location = new System.Drawing.Point(418, 262);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(111, 20);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Create One!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.loginbutton);
+            this.Controls.Add(this.pwtextbox);
+            this.Controls.Add(this.user_textbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Username);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Username_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox user_textbox;
+        private System.Windows.Forms.TextBox pwtextbox;
+        private System.Windows.Forms.Button loginbutton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
