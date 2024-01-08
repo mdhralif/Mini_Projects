34f0185506dc8fb15e676c4d78efbb8cb2da55dd
MD HASIBUR RAHMAN ALIF <122716402+mdhralif@users.noreply.github.com>
2024-01-08 19:08:52 +0600

    Add files via upload

---
+++ Code Refactored/BUS.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generate___Print
+{
+    public class BUS : Ticket
+    {
+        public BUS(int price, string source, string destination/*, color color*/)
+        {
+            this.price = price;
+            this.source = source;
+            this.destination = destination;
+            //this.color = color;
+        }
+        public override void generate_ticket(Traveller traveller)
+        {
+            Console.WriteLine("Generating Ticket for BUS of "+traveller.name);
+        }
+
+        //public override void print_ticket(Ticket ticket,Traveller traveller)
+        //{
+        //    if(color==color.Full_color)
+        //    {
+        //        Console.WriteLine("Traveller name: " + traveller.name + "\nFull_color print");
+        //    }
+        //    else if(color==color.Grayscale)
+        //    {
+        //        Console.WriteLine("Traveller name: " + traveller.name + "\nGrayscale print");
+        //    }
+            
+        //}
+            
+        
+    }
+}
---
+++ Code Refactored/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generate___Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Traveller Rifaf = new Traveller("Rifaf", "male", 22);
+            BUS Greenline = new BUS(500, "Onlie", "khulna");
+            Color_Printer color_Printer = new Color_Printer();
+
+            Booking_Agency booking_Agency = new Booking_Agency();
+            booking_Agency.Ticket_Generator(Greenline, Rifaf);
+            booking_Agency.Ticket_printer(color_Printer, Greenline, Rifaf);
+
+            Console.WriteLine("\n\n");
+
+            BUS Shohag= new BUS(1000, "online", "Dhaka");
+            Traveller Alif = new Traveller("Alif", "male", 21);
+            Grayscale_Printer grayscale_Printer = new Grayscale_Printer();
+            booking_Agency.Ticket_Generator(Shohag, Alif);
+            booking_Agency.Ticket_printer(grayscale_Printer, Shohag,Alif);
+        }
+    }
+}
